82862fbbbe800427fff306f60755a4a173ccebfc
dgoldsb <dgoldsb@live.nl>
2024-12-04 08:20:44 +0100

    Day 4a

--- days/src/days_module/day_04.rs
+++ days/src/days_module/day_04.rs
@@ -8,11 +8,43 @@ impl Day for Day04 {
     }
 
     fn get_index(&self) -> u8 {
-        1
+        4
     }
 
     fn part_a(&self, input: &String) -> String {
-        "".to_string()
+        let input_bytes = input.as_bytes();
+        let line_length: isize = input.find('\n').unwrap() as isize + 1;
+        let mut count = 0;
+
+        for delta in vec![
+            1,
+            -1,
+            line_length,
+            -line_length,
+            line_length + 1,
+            line_length - 1,
+            -line_length + 1,
+            -line_length - 1,
+        ] {
+            for i in 0..input.len() {
+                let x = *input_bytes.get(i).unwrap_or(&0) as char;
+                // Let negative isize overflow because YOLO.
+                let m = *input_bytes
+                    .get((i as isize + delta) as usize)
+                    .unwrap_or(&0u8) as char;
+                let a = *input_bytes
+                    .get((i as isize + 2 * delta) as usize)
+                    .unwrap_or(&0u8) as char;
+                let s = *input_bytes
+                    .get((i as isize + 3 * delta) as usize)
+                    .unwrap_or(&0u8) as char;
+                if x == 'X' && m == 'M' && a == 'A' && s == 'S' {
+                    count += 1;
+                }
+            }
+        }
+
+        count.to_string()
     }
 
     fn part_b(&self, input: &String) -> String {
