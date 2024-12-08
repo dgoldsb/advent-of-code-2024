7fad916bc189598b8c5953ac92ba1c661bea62d3
dgoldsb <dgoldsb@live.nl>
2024-12-08 19:14:09 +0100

    Day 8b

--- days/src/days_module/day_08.rs
+++ days/src/days_module/day_08.rs
@@ -1,11 +1,98 @@
 use crate::days_module::day::Day;
-use helpers::grid::cell::Cell;
 use helpers::grid::grid::Grid;
 use helpers::grid::grid_index::GridIndex;
 use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
 
 pub struct Day08 {}
+
+fn generate_antinodes(a: &GridIndex, b: &GridIndex, grid: &Grid, part_b: bool) -> Vec<GridIndex> {
+    let mut found_nodes = Vec::new();
+
+    let mut a_index = a.clone();
+
+    if part_b {
+        found_nodes.push(a_index);
+    }
+
+    let ax_delta = a.x - b.x;
+    let ay_delta = a.y - b.y;
+    loop {
+        a_index = GridIndex {
+            x: a_index.x + ax_delta,
+            y: a_index.y + ay_delta,
+        };
+
+        if grid.get_cell(&a_index).is_some() {
+            found_nodes.push(a_index);
+        }
+
+        if grid.get_cell(&a_index).is_none() || !part_b {
+            break;
+        }
+    }
+
+    let mut b_index = b.clone();
+
+    if part_b {
+        found_nodes.push(b_index);
+    }
+
+    let bx_delta = b.x - a.x;
+    let by_delta = b.y - a.y;
+    loop {
+        b_index = GridIndex {
+            x: b_index.x + bx_delta,
+            y: b_index.y + by_delta,
+        };
+
+        if grid.get_cell(&b_index).is_some() {
+            found_nodes.push(b_index);
+        }
+
+        if grid.get_cell(&b_index).is_none() || !part_b {
+            break;
+        }
+    }
+
+    found_nodes
+}
+
+fn solve(input: &str, part_b: bool) -> usize {
+    let grid = Grid::from_str(input).unwrap();
+    let mut antennae: HashMap<&char, HashSet<&GridIndex>> = HashMap::new();
+    for cell in grid.iter() {
+        if cell.value == '.' {
+            continue;
+        }
+
+        match antennae.get_mut(&cell.value) {
+            Some(set) => {
+                set.insert(&cell.index);
+            }
+            None => {
+                let mut set = HashSet::new();
+                set.insert(&cell.index);
+                antennae.insert(&cell.value, set);
+            }
+        }
+    }
+
+    let mut antinodes = HashSet::new();
+    antennae.values().for_each(|set| {
+        for a in set {
+            for b in set {
+                if a == b {
+                    continue;
+                }
+
+                antinodes.extend(generate_antinodes(a, b, &grid, part_b));
+            }
+        }
+    });
+
+    antinodes.len()
+}
 
 impl Day for Day08 {
     fn get_id(&self) -> String {
@@ -17,56 +104,11 @@ impl Day for Day08 {
     }
 
     fn part_a(&self, input: &String) -> String {
-        // Signals
-        let grid = Grid::from_str(input).unwrap();
-        let mut antennae: HashMap<&char, HashSet<&GridIndex>> = HashMap::new();
-        for cell in grid.iter() {
-            if cell.value == '.' {
-                continue;
-            }
-
-            match antennae.get_mut(&cell.value) {
-                Some(set) => {
-                    set.insert(&cell.index);
-                }
-                None => {
-                    let mut set = HashSet::new();
-                    set.insert(&cell.index);
-                    antennae.insert(&cell.value, set);
-                }
-            }
-        }
-
-        let mut anti_nodes = HashSet::new();
-
-        for set in antennae.values() {
-            for a in set.iter() {
-                for b in set.iter() {
-                    if a == b {
-                        continue;
-                    }
-                    anti_nodes.insert(GridIndex {
-                        x: a.x + (a.x - b.x),
-                        y: a.y + (a.y - b.y),
-                    });
-                    anti_nodes.insert(GridIndex {
-                        x: b.x + (b.x - a.x),
-                        y: b.y + (b.y - a.y),
-                    });
-                }
-            }
-        }
-
-        anti_nodes
-            .iter()
-            .filter(|i| grid.get_cell(*i).is_some())
-            .collect::<Vec<_>>()
-            .len()
-            .to_string()
+        solve(input, false).to_string()
     }
 
     fn part_b(&self, input: &String) -> String {
-        "".to_string()
+        solve(input, true).to_string()
     }
 }
 
