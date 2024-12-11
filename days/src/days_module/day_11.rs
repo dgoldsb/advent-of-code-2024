637e810758e3293e376e6c914d8bf42c8675c428
dgoldsb <dgoldsb@live.nl>
2024-12-11 19:35:54 +0100

    Took the linked list bait, fixed for 11b

--- days/src/days_module/day_11.rs
+++ days/src/days_module/day_11.rs
@@ -1,53 +1,36 @@
 use crate::days_module::day::Day;
+use std::collections::HashMap;
 
 pub struct Day11 {}
-use std::cell::RefCell;
-use std::rc::Rc;
 
-#[derive(Debug)]
-struct Node<T> {
-    value: T,
-    next: Option<Rc<RefCell<Node<T>>>>,
-    prev: Option<Rc<RefCell<Node<T>>>>,
-}
-
-#[derive(Debug)]
-struct DoublyLinkedList<T> {
-    head: Option<Rc<RefCell<Node<T>>>>,
-    tail: Option<Rc<RefCell<Node<T>>>>,
-    size: usize,
-}
-
-impl<T> DoublyLinkedList<T> {
-    pub fn new() -> Self {
-        Self {
-            head: None,
-            tail: None,
-            size: 0,
-        }
+fn solve(blinks: usize, stone: usize, memory: &mut HashMap<(usize, usize), usize>) -> usize {
+    if blinks == 0 {
+        return 1;
     }
 
-    pub fn push_back(&mut self, value: T) {
-        let new_node = Rc::new(RefCell::new(Node {
-            value,
-            next: None,
-            prev: self.tail.clone(),
-        }));
-
-        match self.tail.take() {
-            Some(old_tail) => {
-                old_tail.borrow_mut().next = Some(new_node.clone());
-                self.tail = Some(new_node);
-            }
-            None => {
-                // List is empty
-                self.head = Some(new_node.clone());
-                self.tail = Some(new_node);
-            }
-        }
-
-        self.size += 1;
+    if memory.contains_key(&(stone, blinks)) {
+        return memory.get(&(stone, blinks)).unwrap().clone();
     }
+
+    let mut result: usize;
+    let stone_string = stone.to_string();
+
+    if stone == 0 {
+        result = solve(blinks - 1, 1, memory);
+    } else if stone_string.len() % 2 == 0 {
+        let first_stone = stone_string[0..stone.to_string().len() / 2]
+            .parse::<usize>()
+            .unwrap();
+        let second_stone = stone_string[stone.to_string().len() / 2..stone_string.len()]
+            .parse::<usize>()
+            .unwrap();
+        result = solve(blinks - 1, first_stone, memory) + solve(blinks - 1, second_stone, memory);
+    } else {
+        result = solve(blinks - 1, stone * 2024, memory);
+    }
+
+    memory.insert((stone, blinks), result.clone());
+    result
 }
 
 impl Day for Day11 {
@@ -59,55 +42,21 @@ impl Day for Day11 {
         11
     }
     fn part_a(&self, input: &String) -> String {
-        // Screams linked list.
-        let mut stones = DoublyLinkedList::new();
+        let mut memory = HashMap::new();
         input
-            .split(" ")
-            .for_each(|x| stones.push_back(x.parse::<usize>().unwrap()));
-
-        for _ in 0..25 {
-            let mut current = stones.head.clone().unwrap();
-            loop {
-                let current_value = current.borrow().value;
-                let next_node = current.borrow().next.clone();
-
-                if current_value == 0 {
-                    current.borrow_mut().value = 1;
-                } else if current_value.to_string().len() % 2 == 0 {
-                    let current_value_string = current_value.to_string();
-                    let first_half = current_value_string[0..current_value_string.len() / 2]
-                        .parse::<usize>()
-                        .unwrap();
-                    let second_half = current_value_string
-                        [current_value_string.len() / 2..current_value_string.len()]
-                        .parse::<usize>()
-                        .unwrap();
-
-                    let new = Node {
-                        value: second_half,
-                        next: current.borrow().next.clone(),
-                        prev: current.borrow().prev.clone(),
-                    };
-                    current.borrow_mut().value = first_half;
-                    current.borrow_mut().next = Some(Rc::new(RefCell::new(new)));
-                    stones.size += 1;
-                } else {
-                    current.borrow_mut().value *= 2024;
-                }
-
-                if next_node.is_none() {
-                    break;
-                } else {
-                    current = next_node.unwrap();
-                }
-            }
-        }
-
-        stones.size.to_string()
+            .split(' ')
+            .map(|s| solve(25, s.parse::<usize>().unwrap(), &mut memory))
+            .sum::<usize>()
+            .to_string()
     }
 
     fn part_b(&self, input: &String) -> String {
-        "".to_string()
+        let mut memory = HashMap::new();
+        input
+            .split(' ')
+            .map(|s| solve(75, s.parse::<usize>().unwrap(), &mut memory))
+            .sum::<usize>()
+            .to_string()
     }
 }
 
