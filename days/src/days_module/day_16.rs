f6a292d152cab2fe79a1bbe5233bd4f394b13532
dgoldsb <dgoldsb@live.nl>
2024-12-16 22:31:35 +0100

    Part 16b

--- days/src/days_module/day_16.rs
+++ days/src/days_module/day_16.rs
@@ -2,6 +2,7 @@ use crate::days_module::day::Day;
 use helpers::grid::grid::Grid;
 use helpers::grid::grid_index::{Direction, GridIndex};
 use lazy_static::lazy_static;
+use std::cell::RefCell;
 use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::str::FromStr;
 
@@ -21,7 +22,11 @@ struct State<'a> {
 }
 
 // State is location + direction, extra cost of 1000 for direction change.
-fn dijkstra(grid: &Grid, start_index: &GridIndex, end_index: &GridIndex) -> usize {
+fn dijkstra<'a>(
+    grid: &'a Grid,
+    start_index: &'a GridIndex,
+    end_index: &'a GridIndex,
+) -> (usize, HashMap<State<'a>, usize>) {
     let mut g_score: HashMap<State, usize> = HashMap::new();
     let mut came_from: HashMap<State, State> = HashMap::new();
     let mut open_set: HashSet<State> = HashSet::new();
@@ -36,11 +41,11 @@ fn dijkstra(grid: &Grid, start_index: &GridIndex, end_index: &GridIndex) -> usiz
     open_set.insert(state);
 
     while !min_heap.is_empty() {
-        let (_, current_state) = min_heap.pop().unwrap();
+        let (current, current_state) = min_heap.pop().unwrap();
         open_set.remove(&current_state);
 
         if current_state.index == end_index {
-            continue;
+            return (usize::MAX - current, g_score);
         }
 
         for direction in DIRECTIONS.iter() {
@@ -82,14 +87,7 @@ fn dijkstra(grid: &Grid, start_index: &GridIndex, end_index: &GridIndex) -> usiz
             }
         }
     }
-
-    g_score
-        .iter()
-        .filter(|(k, _)| k.index == end_index)
-        .map(|(_, v)| v)
-        .min()
-        .unwrap()
-        .clone()
+    panic!("No path found!");
 }
 
 fn pruned_dfs<'a>(
@@ -98,19 +96,55 @@ fn pruned_dfs<'a>(
     current_direction: &'a Direction,
     score: usize,
     max_score: &'a usize,
-    visited: &'a mut HashSet<&'a GridIndex>,
+    visited: &'a RefCell<HashSet<&'a GridIndex>>,
+    g_scores: &'a HashMap<State, usize>,
 ) -> bool {
     if score > *max_score {
         return false;
     }
 
     if grid.get_cell(&current).unwrap().value == 'E' {
-        visited.insert(current);
+        visited.borrow_mut().insert(current); // only for
         return true;
     }
 
+    if score
+        > *g_scores
+            .get(&State {
+                index: current,
+                direction: *current_direction,
+            })
+            .unwrap()
+    {
+        return false;
+    }
+
     let mut any_found = false;
+
     for direction in DIRECTIONS.iter() {
+        match direction {
+            Direction::DOWN => {
+                if *current_direction == Direction::UP {
+                    continue;
+                }
+            }
+            Direction::UP => {
+                if *current_direction == Direction::DOWN {
+                    continue;
+                }
+            }
+            Direction::LEFT => {
+                if *current_direction == Direction::RIGHT {
+                    continue;
+                }
+            }
+            Direction::RIGHT => {
+                if *current_direction == Direction::LEFT {
+                    continue;
+                }
+            }
+        }
+
         let neighbor_cell = grid.move_from_cell(current, &direction).unwrap();
 
         if neighbor_cell.value == '#' {
@@ -122,19 +156,21 @@ fn pruned_dfs<'a>(
             enter_cost += 1000;
         }
 
-        any_found = any_found
-            || pruned_dfs(
-                grid,
-                &neighbor_cell.index,
-                direction,
-                score + enter_cost,
-                max_score,
-                visited,
-            );
+        let result = pruned_dfs(
+            grid,
+            &neighbor_cell.index,
+            direction,
+            score + enter_cost,
+            max_score,
+            visited,
+            g_scores,
+        );
+
+        any_found = any_found || result;
     }
 
     if any_found {
-        visited.insert(current);
+        visited.borrow_mut().insert(current);
         true
     } else {
         false
@@ -155,13 +191,29 @@ impl Day for Day16 {
         let grid = Grid::from_str(input).unwrap();
         let start_index = grid.find_index(&'S').unwrap();
         let end_index = grid.find_index(&'E').unwrap();
-        dijkstra(&grid, start_index, end_index).to_string()
+        dijkstra(&grid, start_index, end_index).0.to_string()
     }
 
     fn part_b(&self, input: &String) -> String {
-        let shortest_path = self.part_a(&input).parse::<usize>().unwrap();
+        let grid = Grid::from_str(input).unwrap();
+        let start_index = grid.find_index(&'S').unwrap();
+        let end_index = grid.find_index(&'E').unwrap();
 
-        "".to_string()
+        let (shortest_path, g_scores) = dijkstra(&grid, start_index, end_index);
+        let visited_set = RefCell::new(HashSet::new());
+
+        pruned_dfs(
+            &grid,
+            start_index,
+            &Direction::RIGHT,
+            0,
+            &shortest_path,
+            &visited_set,
+            &g_scores,
+        );
+
+        let x = visited_set.borrow().len().to_string();
+        x
     }
 }
 
