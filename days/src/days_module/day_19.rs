16caab077c31929109c345b92549122e0d6f2f17
Dylan Goldsborough <dgoldsb@live.nl>
2024-12-19 19:01:11 +0100

    Day 19a

--- days/src/days_module/day_19.rs
+++ days/src/days_module/day_19.rs
@@ -1,6 +1,61 @@
 use crate::days_module::day::Day;
+use std::collections::HashSet;
 
 pub struct Day19 {}
+
+fn count_possible(patterns: &Vec<String>, designs: &Vec<String>) -> usize {
+    let mut seen = HashSet::new();
+    let mut stack = patterns.clone();
+
+    while let Some(tail) = stack.pop() {
+        let found = designs
+            .iter()
+            .any(|p| p == &tail);
+
+        if found {
+            seen.insert(tail.clone());
+        }
+
+        let valid_tail = designs
+            .iter()
+            .any(|p| p.len() >= tail.len() && p[(p.len() - tail.len())..p.len()] == tail[0..tail.len()]);
+
+        if valid_tail {
+            for pattern in patterns {
+                let extended_tail = pattern.to_owned() + &tail;
+                if !seen.contains(&extended_tail) {
+                    seen.insert(extended_tail.clone());
+                    stack.push(extended_tail);
+                }
+            }
+        }
+    }
+
+    designs
+        .iter()
+        .filter(|&s| seen.contains(s))
+        .count()
+}
+
+fn parse(input: &str) -> (Vec<String>, Vec<String>) {
+    let mut input_iter = input.split("\n\n");
+
+    let patterns = input_iter
+        .next()
+        .unwrap()
+        .split(", ")
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    let designs = input_iter
+        .next()
+        .unwrap()
+        .split("\n")
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    return (patterns, designs)
+}
 
 impl Day for Day19 {
     fn get_id(&self) -> String {
@@ -8,10 +63,12 @@ impl Day for Day19 {
     }
 
     fn get_index(&self) -> u8 {
-        1
+        19
     }
+
     fn part_a(&self, input: &String) -> String {
-        "".to_string()
+        let (patterns, designs) = parse(input);
+        count_possible(&patterns, &designs).to_string()
     }
 
     fn part_b(&self, input: &String) -> String {
