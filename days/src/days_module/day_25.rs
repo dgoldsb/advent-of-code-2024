d261c0e2086efcd77605f12d34229dd92b36a8e8
dgoldsb <dgoldsb@live.nl>
2024-12-25 10:01:31 +0100

    Day 25a

--- days/src/days_module/day_25.rs
+++ days/src/days_module/day_25.rs
@@ -1,4 +1,6 @@
 use crate::days_module::day::Day;
+use helpers::grid::grid::Grid;
+use std::str::FromStr;
 
 pub struct Day25 {}
 
@@ -11,7 +13,28 @@ impl Day for Day25 {
         25
     }
     fn part_a(&self, input: &String) -> String {
-        "".to_string()
+        let key_or_locks = input
+            .split("\n\n")
+            .map(|s| Grid::from_str(s).unwrap())
+            .collect::<Vec<Grid>>();
+
+        let mut counter = 0;
+        for a in key_or_locks.iter() {
+            for b in key_or_locks.iter() {
+                let mut overlaps = false;
+                for cell in a.iter() {
+                    if cell.value == '#' && cell.value == b.get_cell(&cell.index).unwrap().value {
+                        overlaps = true;
+                        break;
+                    }
+                }
+                if !overlaps {
+                    counter += 1;
+                }
+            }
+        }
+
+        (counter / 2).to_string()
     }
 
     fn part_b(&self, input: &String) -> String {
